bba563b914cdacb1d9793c7c06d8da37358cfdc3
Fik0u <fikouch@gmail.com>
2025-05-10 11:52:25 +0100

    Background

--- src/App.js
+++ src/App.js
@@ -10,8 +10,21 @@ function App() {
 
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [background, setBackground] = useState('url("/images/default.jpg")');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const weatherBackgrounds = {
+  "clear sky": "url('/images/sunny.jpg')",
+  "few clouds": "url('/images/cloudy.jpg')",
+  "scattered clouds": "url('/images/cloudy.jpg')",
+  "broken clouds": "url('/images/brokenClouds.jpg')",
+  "shower rain": "url('/images/rainy.jpg')",
+  "rain": "url('/images/rainy.jpg')",
+  "thunderstorm": "url('/images/thunderstorm.jpg')",
+  "snow": "url('/images/snow.jpg')",
+  "mist": "url('/images/foggy.jpg')",
+};
 
   const handleSearch = async (cityName) => {
     setCity(cityName);
@@ -22,7 +35,10 @@ function App() {
       const result = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric&lang=en`);
         
-        setWeatherData(result.data)
+        setWeatherData(result.data);
+
+        const weatherDescription = result.data.weather[0].description;
+        setBackground(weatherBackgrounds[weatherDescription] || 'url("/images/default.jpg")');
     } catch (error) {
       setError(error.response ? error.response.data.message : error.message)
     } finally {
@@ -31,8 +47,7 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Weather Now</h1>
+    <div className="App" style={{ backgroundImage: background }}>
 
       {loading && <SpinLoad />}
 
